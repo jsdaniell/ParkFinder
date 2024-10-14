a7037382d4f66c3de07ce8b0acfde4dbc6b5dfa7
José Daniel <jose.daniell@outlook.com>
2024-10-14 18:49:16 -0300

    aDDED icons

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,9 @@
 export default function SideBar(){
     return (
-        <div className="absolute left-0 min-h-screen min-w-24 p-2 flex justify-center bg-blue-950 text-white">
-                A
+        <div className="absolute left-0 min-h-screen min-w-24 p-2 flex 
+        justify-start items-center bg-blue-950 text-white flex-col">
+                <div>A</div>
+                <div>A</div>
         </div>
     )
 }
